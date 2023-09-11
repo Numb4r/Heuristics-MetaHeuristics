9a75937c59b7a5b91fa16d410aa2ab00d2909369
Yuri Dimitre Dias de Faria <yuri.faria@enacom.com.br>
2023-09-10 21:56:13 -0300

    fix bug with shuffle list, improve docstring

--- construct_heuristics/random_heuristic.py
+++ construct_heuristics/random_heuristic.py
@@ -1,15 +1,22 @@
 from tsp import TSP 
 import random 
 
-def random_heuristic(problem:TSP,rng=random):
-    """Random heuristic for TSP problem
-    param:
-    (TSP) problem: TSP problem model
-    ([Optional] random): Random number generator engine 
-    return:
-    (List[int]) list of the order of nodes 
+def random_heuristic(problem: TSP, rng=random):
+    """
+    Random heuristic for the Traveling Salesman Problem (TSP).
+
+    This heuristic generates a random order of nodes as a possible solution to the TSP problem.
+
+    Args:
+        problem (TSP): The TSP problem model.
+        rng (Optional[random]): Random number generator engine (default: random module).
+
+    Returns:
+        List[int]: A list representing the order of nodes in the generated solution.
+
+    Example:
+        solution = random_heuristic(tsp_instance)
     """
     nodes = problem.nodes()
-    nodes.shuffle(rng)
+    rng.shuffle(nodes)
     return nodes
-    
